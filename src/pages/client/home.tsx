bc0414b5c88b3b042851155c6ee34857e1ef7a96
Le Son <levanson2004dn@gmail.com>
2025-11-01 22:33:53 +0700

    sửa hiển thị book

--- src/pages/client/home.tsx
+++ src/pages/client/home.tsx
@@ -18,7 +18,7 @@ import {
 } from "antd";
 import { ShoppingCartOutlined, UserOutlined } from "@ant-design/icons";
 import "@/styles/home.scss";
-import type { PaginationProps } from "antd";
+import type { PaginationProps, CheckboxOptionType, GetProp } from "antd";
 import { Flex, Spin } from "antd";
 
 import { getBookAPI, getCategoryAPI } from "@/services/api";
@@ -27,45 +27,57 @@ const { Content } = Layout;
 
 const HomePage = () => {
   const [dataBook, setDataBook] = useState<IBooks[]>([]);
-  const [dataCategory, setDataCategory] = useState<string[]>([]);
-  const [totalPage, setTotalPage] = useState<number>(10);
+  const [dataCategory, setDataCategory] = useState<
+    { label: string; value: string }[]
+  >([]);
+  const [totalPage, setTotalPage] = useState<number>(0);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [pageSizeState, setPageSizeState] = useState<number>(5);
   useEffect(() => {
     const fetchCategory = async () => {
       const res = await getCategoryAPI();
       if (res && res.data) {
-        setDataCategory(res.data);
+        const d = res.data.map((item) => {
+          return { label: item, value: item };
+        });
+        setDataCategory(d);
       }
     };
     fetchCategory();
   }, []);
   useEffect(() => {
-    let query = "";
-    const fetchBook = async () => {
-      query += `?current=1&pageSize=5`;
-      const res = await getBookAPI(query);
-      if (res && res.data) {
-        setDataBook(res.data.result);
-        setTotalPage(res.data.meta.total);
-      } else {
-        message.error(res.message);
-      }
-    };
     fetchBook();
-  }, []);
-  const onShowSizeChange: PaginationProps["onShowSizeChange"] = async (
-    current,
-    pageSize
-  ) => {
+  }, [currentPage, pageSizeState]);
+  const fetchBook = async () => {
     setIsLoading(true);
-    let query = `?current=${current}&pageSize=${pageSize}`;
+    let query = `?current=${currentPage}&pageSize=${pageSizeState}`;
     const res = await getBookAPI(query);
     if (res && res.data) {
       setDataBook(res.data.result);
+      setTotalPage(res.data.meta.total);
     } else {
       message.error(res.message);
     }
     setIsLoading(false);
+  };
+  //page
+  const onShowSizeChange: PaginationProps["onShowSizeChange"] = async (
+    current,
+    pageSize
+  ) => {
+    if (current !== currentPage) {
+      setCurrentPage(current);
+    }
+    if (pageSize !== pageSizeState) {
+      setPageSizeState(pageSize);
+    }
+  };
+  //select box
+  const onChange: GetProp<typeof Checkbox.Group, "onChange"> = (
+    checkedValues
+  ) => {
+    console.log("checked = ", checkedValues);
   };
   return (
     <Layout className="bookstore-layout">
@@ -76,13 +88,14 @@ const HomePage = () => {
           <h4>Bộ lọc tìm kiếm</h4>
           <Divider />
           <div>Danh mục sản phẩm</div>
-          <Checkbox.Group style={{ display: "flex", flexDirection: "column" }}>
-            {dataCategory.map((item) => (
-              <Checkbox key={item} value={item} style={{ marginTop: 8 }}>
-                {item}
-              </Checkbox>
-            ))}
-          </Checkbox.Group>
+
+          <Checkbox.Group
+            options={dataCategory}
+            defaultValue={["Pear"]}
+            onChange={onChange}
+            // style={{ display: "block", marginRight: 0 }}
+          />
+
           <Divider />
           <div>Khoảng giá</div>
           <div className="price-filter">
@@ -107,9 +120,38 @@ const HomePage = () => {
           />
 
           {isLoading ? (
-            <Flex align="center" gap="middle">
-              <Spin size="large" />
-            </Flex>
+            <Spin tip="Loading" size="large" spinning={isLoading}>
+              <Row gutter={[24, 24]}>
+                {dataBook.map((b, idx) => (
+                  <Col key={idx} xs={24} sm={12} md={8} lg={6} xl={4}>
+                    <Card
+                      hoverable
+                      className="book-card"
+                      cover={
+                        <img
+                          alt={b.mainText}
+                          src={`${
+                            import.meta.env.VITE_BACKEND_URL
+                          }/images/book/${b.thumbnail}`}
+                          className="book-image"
+                        />
+                      }
+                    >
+                      <div className="book-title">{b.mainText}</div>
+                      <div className="book-price">
+                        {b.price.toLocaleString()} đ
+                      </div>
+                      <Rate
+                        disabled
+                        defaultValue={5}
+                        style={{ fontSize: 12 }}
+                      />
+                      <div className="book-sold">Đã bán {b.sold}</div>
+                    </Card>
+                  </Col>
+                ))}
+              </Row>
+            </Spin>
           ) : (
             <Row gutter={[24, 24]}>
               {dataBook.map((b, idx) => (
